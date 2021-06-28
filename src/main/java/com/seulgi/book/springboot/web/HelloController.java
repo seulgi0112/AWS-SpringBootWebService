33378732bd3357874d8856949122884eb9e414e5
seulgi0112 <sara629718@gmail.com>
2021-06-28 16:54:54 +0900

    init🎉

--- src/main/java/com/seulgi/book/springboot/web/HelloController.java
+++ src/main/java/com/seulgi/book/springboot/web/HelloController.java
@@ -1,5 +1,14 @@
 package com.seulgi.book.springboot.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.seulgi.book.springboot.web.dto.HelloResponseDto;
+
 public class HelloController {
+	@GetMapping
+	public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+		return new HelloResponseDto(name, amount);
+	}
 
 }
